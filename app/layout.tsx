5525be31ff3b16319b416466c1e757daf88c054f
Joaquipe13 <jfernandezd@frro.utn.edu.ar>
2024-10-21 20:09:01 -0300

    optimizaciones

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-
 import "./globals.css";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
--- app/upload-image/page.tsx
+++ app/upload-image/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 export default function MenuPage() {
   const [image, setImage] = useState<File | null>(null);
   const [imageUrl, setImageUrl] = useState<string | null>(null);
-  const [message, setMessage] = useState("");
   const router = useRouter();
 
   const handleImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -28,14 +28,14 @@ export default function MenuPage() {
 
       if (response.ok) {
         const data = await response.json();
-        setMessage(data.message);
+        console.log(data.message);
         setImageUrl(data.imageUrl);
       } else {
         const errorData = await response.json();
-        setMessage(errorData.message || "Error uploading image");
+        console.log(errorData.message || "Error uploading image");
       }
     } else {
-      setMessage("Please select an image to upload");
+      console.log("Please select an image to upload");
     }
   };
   const handleClick = () => {
@@ -44,51 +44,50 @@ export default function MenuPage() {
 
   return (
     <div className="max-w-md mx-auto p-6 bg-gray-900 rounded-lg shadow-lg">
-    <br />
-    <h1 className="text-center text-2xl text-white font-semibold mb-4">
-      Upload Image
-    </h1>
-    
-    {imageUrl && (
-      <div className="mt-4">
-        <h2 className="text-center text-xl text-white font-medium mb-4"></h2>
-        <img
-          src={"images/" + imageUrl}
-          alt="Uploaded"
-          className="mx-auto rounded-md shadow-md"
-        />
-      </div>
-    )}
-    
-    <br />
-    <h2 className="text-center text-xl text-white font-medium mb-6">
-      Upload new image
-    </h2>
-    
-    <input
-      type="file"
-      accept="image/*"
-      onChange={handleImageUpload}
-      className="block w-full text-sm text-gray-300 border border-gray-500 rounded-lg cursor-pointer bg-gray-800 focus:outline-none focus:border-blue-500 p-2 mb-4"
-    />
-    
-    <br />
-    <button
-      onClick={handleUpload}
-      className="w-full bg-blue-600 text-white font-semibold p-3 rounded-md hover:bg-blue-500 transition-colors"
-    >
-      Upload
-    </button>
-    
-    <br />
-    <br />
-    <button
-      onClick={handleClick}
-      className="w-full bg-blue-600 text-white font-semibold p-3 rounded-md hover:bg-blue-500 transition-colors"
-    >
-      View all images
-    </button>
-  </div>
-  
+      <br />
+      <h1 className="text-center text-2xl text-white font-semibold mb-4">
+        Upload Image
+      </h1>
+
+      {imageUrl && (
+        <div className="mt-4">
+          <h2 className="text-center text-xl text-white font-medium mb-4"></h2>
+          <Image
+            src={"images/" + imageUrl}
+            alt="Uploaded"
+            className="mx-auto rounded-md shadow-md"
+          />
+        </div>
+      )}
+
+      <br />
+      <h2 className="text-center text-xl text-white font-medium mb-6">
+        Upload new image
+      </h2>
+
+      <input
+        type="file"
+        accept="image/*"
+        onChange={handleImageUpload}
+        className="block w-full text-sm text-gray-300 border border-gray-500 rounded-lg cursor-pointer bg-gray-800 focus:outline-none focus:border-blue-500 p-2 mb-4"
+      />
+
+      <br />
+      <button
+        onClick={handleUpload}
+        className="w-full bg-blue-600 text-white font-semibold p-3 rounded-md hover:bg-blue-500 transition-colors"
+      >
+        Upload
+      </button>
+
+      <br />
+      <br />
+      <button
+        onClick={handleClick}
+        className="w-full bg-blue-600 text-white font-semibold p-3 rounded-md hover:bg-blue-500 transition-colors"
+      >
+        View all images
+      </button>
+    </div>
   );
 }
